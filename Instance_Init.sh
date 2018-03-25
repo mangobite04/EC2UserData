27718180b17ad7c8937bf119f4106fa729f12856
mangobite04 <devesh.rai04@gmail.com>
2018-03-25 13:59:34 +0000

    Update Instance_Init.sh
    Update with Maven

--- Instance_Init.sh
+++ Instance_Init.sh
@@ -11,14 +11,14 @@ yum install -y wget
 yum install -y curl
 
 # Remove current apache & php 
-yum remove httpd* php*
+yum remove httpd* php* -y
 
 # Install Apache 2.4
-yum install httpd24
+yum install httpd24 -y
 
 # Install PHP 7.0 
 # automatically includes php70-cli php70-common php70-json php70-process php70-xml
-yum install php70
+yum install php70 -y
 
 # Install additional commonly used php packages
 yum install php70-gd -y
@@ -29,36 +29,8 @@ yum install php70-opcache -y
 yum install php70-pdo -y
 yum install php70-pecl-apcu -y
 
-# Change Directory to Local
-cd /usr/local
-
-# Download apache-maven file
-wget http://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
-
-# Untar apache-maven file
-/usr/bin/tar xzf apache-maven-3.5.2-bin.tar.gz
-
-# Linking of maven
-/usr/bin/ln -s apache-maven-3.5.2  maven
-
-# Setup Maven Path
-/usr/bin/echo export M2_HOME=/usr/local/maven > /etc/profile.d/maven.sh
-/usr/bin/echo export PATH=${M2_HOME}/bin:${PATH} >> /etc/profile.d/maven.sh
-
-# Run Maven
-source /etc/profile.d/maven.sh
-
 # Remove download apache-maven file from directory
 rm -f /usr/local/apache-maven-3.5.2-bin.tar.gz
-
-# Chnage directory
-cd /root
-
-# Cloaning of Project - Relay24
-git clone https://github.com/lc-nyovchev/opstest.git
-cd /root/opstest
-nohup ./mvnw spring-boot:run -Dspring.config.location=/root/opstest/src/main/resources/application.properties &
-echo ""
 
 # Creating PHP file for Project - Relay24
 cat > /var/www/html/index.php << "EOFF"
@@ -88,7 +60,7 @@ cat > /var/www/html/index.php << "EOFF"
   </html>
 EOFF
 echo ""
-chmod 0644 /var/www/html/index.php
+chmod +x /var/www/html/index.php
 
 # HTTPD Services Start
 /sbin/service httpd start
@@ -96,3 +68,27 @@ chmod 0644 /var/www/html/index.php
 # Adding in config file to start after server reboot
 /sbin/chkconfig --add httpd
 /sbin/chkconfig httpd on
+
+# Change Directory to Local
+cd /usr/local
+
+# Download apache-maven file
+wget http://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
+
+# untar apache-maven file
+/usr/bin/tar xzf apache-maven-3.5.2-bin.tar.gz
+
+# Linking of maven
+/usr/bin/ln -s apache-maven-3.5.2  maven
+
+# Setup Maven Path
+/usr/bin/echo export M2_HOME=/usr/local/maven > /etc/profile.d/maven.sh
+/usr/bin/echo export PATH=${M2_HOME}/bin:${PATH} >> /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
+
+# Cloaning of Project - Relay24
+cd /root
+git clone https://github.com/lc-nyovchev/opstest.git
+cd /root/opstestnohup 
+./mvnw spring-boot:run -Dspring.config.location=/root/opstest/src/main/resources/application.properties &
+echo ""
